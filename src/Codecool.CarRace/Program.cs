64ebc3d4fc761419ab3e0937c1a7071d178a0473
KH <kh@wp.pl>
2021-03-05 12:14:56 +0100

    Init.

---
+++ src/Codecool.CarRace/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Codecool.CarRace
+{
+    /// <summary>
+    /// This is the main class of your program which contains Main method
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// This is the entry point of your program.
+        /// </summary>
+        /// <param name="args">Command line arguments. It is unused for this program.</param>
+        public static void Main(string[] args)
+        {
+            Race race = new Race();
+            CreateVehicles(race);
+
+            race.SimulateRace();
+            race.PrintRaceResults();
+        }
+
+        /// <summary>
+        /// Creates all the vehicles that will be part of this race.
+        /// </summary>
+        /// <param name="race">The race where these vehiles will participate</param>
+        private static void CreateVehicles(Race race)
+        {
+        }
+    }
+}
---
+++ src/Codecool.CarRace/Race.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Codecool.CarRace
+{
+    /// <summary>
+    /// This class has methods with different forms of greetings.
+    /// </summary>
+    public class Race
+    {
+        /// <summary>
+        /// Simulates the passing of time by advancing the weather and
+        /// moving the vehicles for the duration of a whole race.
+        /// </summary>
+        public void SimulateRace()
+        {
+        }
+
+        /// <summary>
+        /// Prints the state of all vehicles. Called at the end of the
+        /// race.
+        /// </summary>
+        public void PrintRaceResults()
+        {
+            Console.WriteLine("Race results");
+        }
+    }
+}
